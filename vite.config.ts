1fde9e3c30620c666ed39f66d6da4921df3c5555
wills <bigredwill@gmail.com>
2025-08-28 14:19:50 -0700

    ts cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import { join, resolve } from "node:path";
-import { fileURLToPath } from "node:url";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
